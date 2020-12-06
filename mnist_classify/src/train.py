3229386e9de4e4358a51ad9cea72b6f4e6112a1c
Hien Hoang <hien.hoang@tpptechnology.com>
2020-12-06 23:46:34 +0700

    feat: Add model dispatcher

--- mnist_classify/src/train.py
+++ mnist_classify/src/train.py
@@ -5,9 +5,10 @@ import pandas as pd
 from sklearn import metrics
 from sklearn import tree
 import config
+import model_dispatcher
 
 
-def run(fold):
+def run(fold, model):
     df = pd.read_csv(config.INPUT_FILE)
 
     df_train = df[df.kfold != fold].reset_index(drop=True)
@@ -19,7 +20,7 @@ def run(fold):
     x_valid = df_valid.drop('target', axis=1).values
     y_valid = df_valid.target.values
 
-    clf = tree.DecisionTreeClassifier()
+    clf = model_dispatcher.models[model]
     clf.fit(x_train, y_train)
 
     preds = clf.predict(x_valid)
@@ -36,5 +37,9 @@ if __name__ == "__main__":
         "--fold",
         type=int
     )
+    parser.add_argument(
+        "--model",
+        type=str
+    )
     args = parser.parse_args()
-    run(fold=args.fold)
+    run(fold=args.fold, model=args.model)
